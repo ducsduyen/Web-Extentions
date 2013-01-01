b79bf67eb012deabd6dfbec9ba1b32ee82123243
ducsduyen <ducsduyen@gmail.com>
2013-01-01 11:09:15 -0800

    Create jquery.slug.js

---
+++ jquery.slug.js
@@ -0,0 +1,17 @@
+/*jquery.slug v1.0 | author:ducsduyen | license:MIT*/
+$.fn.slug = function()
+{
+    return this.val()
+        .replace(/[áàảãạâấầẩẫậăắằẳẵặ]/ig,'a')
+        .replace(/[đ]/ig,'d')
+        .replace(/[éèẻẽẹêếềểễệ]/ig,'e')
+        .replace(/[íìỉĩị]/ig,'i')
+        .replace(/[óòỏõọôốồổỗộơớờởỡợ]/ig,'o')
+        .replace(/[ýỳỷỹỵ]/ig,'y')
+        .replace(/[úùủũụưứừửữự]/ig,'u')
+        .replace(/\s+/ig,'-')
+        .replace(/[-]+/ig,'-')
+        .replace(/^[-]+|[-]+$/ig, '')
+        .replace(/[^a-z0-9-]/ig, '')
+        .toLowerCase();
+}
