82bc09014a2594efe58bed7c4bcf66c02173c6da
ducsduyen <ducsduyen@gmail.com>
2015-12-22 09:54:30 +0700

    Update jquery.slug.js

--- jquery.slug.js
+++ jquery.slug.js
@@ -9,8 +9,8 @@ $.fn.slug = function()
        .replace(/[óòỏõọôốồổỗộơớờởỡợ]/ig, 'o')
        .replace(/[ýỳỷỹỵ]/ig, 'y')
        .replace(/[úùủũụưứừửữự]/ig, 'u')
-       .replace(/[^a-z0-9-]/ig, '-')
        .replace(/\s+|[-]+/ig, '-')
-       .replace(/^[-]+|[-]+$|^\s+|\s+$/ig, '')       
+       .replace(/^[-]+|[-]+$|^\s+|\s+$/ig, '')
+       .replace(/[^a-z0-9-]/ig, '')
        .toLowerCase();
 }
