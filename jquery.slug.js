feb8c02fedf6c04d9c44013d7e4e5a7a16fcc9d6
ducsduyen <ducsduyen@gmail.com>
2015-12-22 09:44:13 +0700

    Update jquery.slug.js

--- jquery.slug.js
+++ jquery.slug.js
@@ -1,17 +1,16 @@
 /*jquery.slug v1.0 | author:ducsduyen | license:MIT*/
 $.fn.slug = function()
 {
-    return this.val()
-        .replace(/[áàảãạâấầẩẫậăắằẳẵặ]/ig,'a')
-        .replace(/[đ]/ig,'d')
-        .replace(/[éèẻẽẹêếềểễệ]/ig,'e')
-        .replace(/[íìỉĩị]/ig,'i')
-        .replace(/[óòỏõọôốồổỗộơớờởỡợ]/ig,'o')
-        .replace(/[ýỳỷỹỵ]/ig,'y')
-        .replace(/[úùủũụưứừửữự]/ig,'u')
-        .replace(/\s+/ig,'-')
-        .replace(/[-]+/ig,'-')
-        .replace(/^[-]+|[-]+$/ig, '')
-        .replace(/[^a-z0-9-]/ig, '')
-        .toLowerCase();
+    return this.val() 
+       .replace(/[áàảãạâấầẩẫậăắằẳẵặ]/ig, 'a')
+       .replace(/[đ]/ig, 'd')
+       .replace(/[éèẻẽẹêếềểễệ]/ig, 'e')
+       .replace(/[íìỉĩị]/ig, 'i')
+       .replace(/[óòỏõọôốồổỗộơớờởỡợ]/ig, 'o')
+       .replace(/[ýỳỷỹỵ]/ig, 'y')
+       .replace(/[úùủũụưứừửữự]/ig, 'u')
+	   .replace(/[^a-z0-9-]/ig, '-')
+       .replace(/\s+|[-]+/ig, '-')
+       .replace(/^[-]+|[-]+$|^\s+|\s+$/ig, '')       
+       .toLowerCase();
 }
